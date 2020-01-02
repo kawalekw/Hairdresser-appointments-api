a4a5a265bc2dc6faa97ace507db8e12980849c2a
Mateusz Tokarczyk <mati1000500100900@gmail.com>
2020-01-02 18:11:13 +0100

    Initial commit

---
+++ src/main/java/com/javaretards/hairdresserapponintments/HairdresserApponintmentsApplication.java
@@ -0,0 +1,13 @@
+package com.javaretards.hairdresserapponintments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairdresserApponintmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairdresserApponintmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaretards/hairdresserapponintments/HairdresserApponintmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaretards.hairdresserapponintments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairdresserApponintmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
