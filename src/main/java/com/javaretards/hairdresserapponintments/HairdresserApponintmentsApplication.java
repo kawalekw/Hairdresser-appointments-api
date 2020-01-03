4e8ebe36b625ef018b66f7d09a26a8b772493deb
mateusz <mati1000500100900@gmail.com>
2020-01-03 19:18:24 +0100

    Packages structure fix

--- src/main/java/com/javaretards/hairdresserapponintments/HairdresserApponintmentsApplication.java
+++ src/main/java/com/javaretards/hairdresserapponintments/HairdresserApponintmentsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HairdresserApponintmentsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HairdresserApponintmentsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HairdresserApponintmentsApplication.class, args);
+    }
 }
