b8047cf4df2236ef5d835f7f402b4fc741ddc048
mateusz <mati1000500100900@gmail.com>
2020-01-08 00:02:26 +0100

    Added client blocking feature

--- src/main/java/com/javaretards/hairdresserapponintments/Controller/ConfigController.java
+++ src/main/java/com/javaretards/hairdresserapponintments/Controller/ConfigController.java
@@ -1,8 +1,11 @@
 package com.javaretards.hairdresserapponintments.Controller;
 
+import com.javaretards.hairdresserapponintments.Entity.Client;
 import com.javaretards.hairdresserapponintments.Entity.ServiceOption;
+import com.javaretards.hairdresserapponintments.Repository.ClientRepository;
 import com.javaretards.hairdresserapponintments.Repository.OpenHoursRepositiory;
 import com.javaretards.hairdresserapponintments.Repository.ServiceRepository;
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +28,8 @@ public class ConfigController {
     ServiceRepository sr;
     @Autowired
     OpenHoursRepositiory ohr;
+    @Autowired
+    ClientRepository cr;
     
     @RequestMapping(value = "/services", method = RequestMethod.GET)
     public String viewServicesAction(Model model){
@@ -53,5 +58,33 @@ public class ConfigController {
     public String changeopenHoursAction(Model model){
         model.addAttribute("openhours", ohr.findFirstByAppliesFromBeforeOrderByAppliesFromDesc(LocalDate.now().plusDays(1)).get());
         return "openhours";
+    }
+    
+    @RequestMapping(value = "/clients", method = RequestMethod.GET)
+    public String clientsAction(Model model){
+        model.addAttribute("clients", cr.findAll());
+        return "clients";
+    }
+    
+    @RequestMapping(value = "/clients/block/{id}", method = RequestMethod.GET)
+    public String blockClientAction(@PathVariable("id") Long id){
+        Optional<Client> toDelete = cr.findById(id);
+        if(toDelete.isPresent()){
+            Client cl = toDelete.get();
+            cl.block();
+            cr.save(cl);
+        }
+        return "redirect:/config/clients";
+    }
+    
+    @RequestMapping(value = "/clients/unlock/{id}", method = RequestMethod.GET)
+    public String unlockClientAction(@PathVariable("id") Long id){
+        Optional<Client> toDelete = cr.findById(id);
+        if(toDelete.isPresent()){
+            Client cl = toDelete.get();
+            cl.unlock();
+            cr.save(cl);
+        }
+        return "redirect:/config/clients";
     }
 }
--- src/main/java/com/javaretards/hairdresserapponintments/Entity/Client.java
+++ src/main/java/com/javaretards/hairdresserapponintments/Entity/Client.java
@@ -14,19 +14,27 @@ import lombok.NoArgsConstructor;
  * @author mateusz
  */
 @Entity
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Getter
     @Column(unique=true)
     private String phone;
-    @Getter
     private boolean blocked;
+
+    public Client(String phone) {
+        this.phone = phone;
+        this.blocked = false;
+    }
     
     public void block(){
         this.blocked=true;
+    }
+    
+    public void unlock(){
+        this.blocked=false;
     }
 }
